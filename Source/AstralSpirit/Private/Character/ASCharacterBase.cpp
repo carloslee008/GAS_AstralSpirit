273f06ef2e5bd7670503a24361d73dd82fdd5261
carloslee008 <59149514+carloslee008@users.noreply.github.com>
2024-08-28 03:03:09 -0400

    Character Blueprint Setup

--- Source/AstralSpirit/Private/Character/ASCharacterBase.cpp
+++ Source/AstralSpirit/Private/Character/ASCharacterBase.cpp
@@ -7,6 +7,10 @@ AASCharacterBase::AASCharacterBase()
 {
 	PrimaryActorTick.bCanEverTick = false;
 
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
 }
 
 void AASCharacterBase::BeginPlay()
