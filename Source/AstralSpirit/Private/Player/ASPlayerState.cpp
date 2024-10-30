f1933bdd88a7077e8bf29e689254868656cc1b57
carloslee008 <59149514+carloslee008@users.noreply.github.com>
2024-10-30 00:42:19 -0400

    Player State

---
+++ Source/AstralSpirit/Private/Player/ASPlayerState.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/ASPlayerState.h"
+
+AASPlayerState::AASPlayerState()
+{
+	NetUpdateFrequency = 100.f;
+}
---
+++ Source/AstralSpirit/Public/Player/ASPlayerState.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "ASPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASTRALSPIRIT_API AASPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+public:
+	AASPlayerState();
+};
