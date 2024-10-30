d4b6a70b8a17975cd7a7500cf143991bae36b462
carloslee008 <59149514+carloslee008@users.noreply.github.com>
2024-10-30 00:59:59 -0400

    Ability System Component, Attribute Set

---
+++ Source/AstralSpirit/Public/AbilitySystem/ASAbilitySystemComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
+#include "ASAbilitySystemComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASTRALSPIRIT_API UASAbilitySystemComponent : public UAbilitySystemComponent
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/AstralSpirit/Public/AbilitySystem/ASAttributeSet.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "ASAttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASTRALSPIRIT_API UASAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+};
