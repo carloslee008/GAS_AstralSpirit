45a98fa121f79be4b4be03c778c124caba7ca609
carloslee008 <59149514+carloslee008@users.noreply.github.com>
2024-11-19 22:39:31 -0500

    Replacing Callbacks with Lambdas

--- Source/AstralSpirit/Private/UI/WidgetController/OverlayWidgetController.cpp
+++ Source/AstralSpirit/Private/UI/WidgetController/OverlayWidgetController.cpp
@@ -17,29 +17,33 @@ void UOverlayWidgetController::BroadcastInitialValues()
 	
 }
 
+// Configure Broadcast events to update the overlay widget components
 void UOverlayWidgetController::BindCallbacksToDependencies()
 {
 	const UASAttributeSet* ASAttributeSet = CastChecked<UASAttributeSet>(AttributeSet);
 
-	AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(
-		ASAttributeSet->GetHealthAttribute()).AddUObject(this, &UOverlayWidgetController::HealthChanged);
-
-	AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(
-		ASAttributeSet->GetMaxHealthAttribute()).AddUObject(this, &UOverlayWidgetController::MaxHealthChanged);
-
-	AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(
-		ASAttributeSet->GetManaAttribute()).AddUObject(this, &UOverlayWidgetController::ManaChanged);
-
-	AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(
-		ASAttributeSet->GetMaxManaAttribute()).AddUObject(this, &UOverlayWidgetController::MaxManaChanged);
-
+	// Vital Attributes
+	
+	// Health
+	BindAttributeChange(ASAttributeSet->GetHealthAttribute(), &OnHealthChanged);
+	
+	// Max Health
+	BindAttributeChange(ASAttributeSet->GetMaxHealthAttribute(), &OnMaxHealthChanged);
+	
+	// Mana
+	BindAttributeChange(ASAttributeSet->GetManaAttribute(), &OnManaChanged);
+	
+	// Max Mana
+	BindAttributeChange(ASAttributeSet->GetMaxManaAttribute(), &OnMaxManaChanged);
+	
 	Cast<UASAbilitySystemComponent>(AbilitySystemComponent)->EffectAssetTags.AddLambda(
 		[this](const FGameplayTagContainer& AssetTags)
 		{
 			for (const FGameplayTag& Tag : AssetTags)
 			{
 				// For example, let's say Tag = Message.HealthPotion
-				// "Message.HealthPotion".MatchesTag("Message") will return True, "Message".MatchesTag("Message.HealthPotion") will return False
+				// "Message.HealthPotion".MatchesTag("Message") will return True,
+				// "Message".MatchesTag("Message.HealthPotion") will return False
 				FGameplayTag MessageTag = FGameplayTag::RequestGameplayTag(FName("Message"));
 				if (Tag.MatchesTag(MessageTag))
 				{
@@ -51,22 +55,13 @@ void UOverlayWidgetController::BindCallbacksToDependencies()
 	);
 }
 
-void UOverlayWidgetController::HealthChanged(const FOnAttributeChangeData& Data) const
+void UOverlayWidgetController::BindAttributeChange(const FGameplayAttribute& Attribute,
+	FOnAttributeChangedSignature* AttributeChangeDelegate) const
 {
-	OnHealthChanged.Broadcast(Data.NewValue);
-}
-
-void UOverlayWidgetController::MaxHealthChanged(const FOnAttributeChangeData& Data) const
-{
-	OnMaxHealthChanged.Broadcast(Data.NewValue);
-}
-
-void UOverlayWidgetController::ManaChanged(const FOnAttributeChangeData& Data) const
-{
-	OnManaChanged.Broadcast(Data.NewValue);
-}
-
-void UOverlayWidgetController::MaxManaChanged(const FOnAttributeChangeData& Data) const
-{
-	OnMaxManaChanged.Broadcast(Data.NewValue);
+	AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(Attribute).AddLambda(
+		[AttributeChangeDelegate](const FOnAttributeChangeData& Data)
+		{
+			AttributeChangeDelegate->Broadcast(Data.NewValue);
+		}
+	);
 }
