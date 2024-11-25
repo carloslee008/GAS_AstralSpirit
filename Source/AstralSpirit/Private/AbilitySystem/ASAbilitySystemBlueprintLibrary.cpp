31efb304dff7e6f4004061d84fd7cdf8331e9278
carloslee008 <59149514+carloslee008@users.noreply.github.com>
2024-11-24 22:58:57 -0500

    AS Ability System Blueprint Library

---
+++ Source/AstralSpirit/Private/AbilitySystem/ASAbilitySystemBlueprintLibrary.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AbilitySystem/ASAbilitySystemBlueprintLibrary.h"
+
+#include "Kismet/GameplayStatics.h"
+#include "Player/ASPlayerState.h"
+#include "UI/HUD/ASHUD.h"
+#include "UI/WidgetController/ASWidgetController.h"
+
+UOverlayWidgetController* UASAbilitySystemBlueprintLibrary::GetOverlayWidgetController(
+	const UObject* WorldContextObject)
+{
+	if (APlayerController* PC = UGameplayStatics::GetPlayerController(WorldContextObject, 0))
+	{
+		if (AASHUD* ASHUD = Cast<AASHUD>(PC->GetHUD()))
+		{
+			AASPlayerState* PS = PC->GetPlayerState<AASPlayerState>();
+			UAbilitySystemComponent* ASC = PS->GetAbilitySystemComponent();
+			UAttributeSet* AS = PS->GetAttributeSet();
+			const FWidgetControllerParams WidgetControllerParams(PC, PS, ASC, AS);
+			return ASHUD->GetOverlayWidgetController(WidgetControllerParams);
+		}	
+	}
+	return nullptr;
+}
---
+++ Source/AstralSpirit/Public/AbilitySystem/ASAbilitySystemBlueprintLibrary.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "ASAbilitySystemBlueprintLibrary.generated.h"
+
+class UOverlayWidgetController;
+/**
+ * 
+ */
+UCLASS()
+class ASTRALSPIRIT_API UASAbilitySystemBlueprintLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+public:
+
+	UFUNCTION(BlueprintPure, Category="ASAbilitySystemBlueprintLibrary|WidgetController", meta=(WorldContext="WorldContextObject"))
+	static UOverlayWidgetController* GetOverlayWidgetController(const UObject* WorldContextObject);
+};
