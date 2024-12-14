13334c82b0f896225d5b690eb0592235d8b12673
carloslee008 <59149514+carloslee008@users.noreply.github.com>
2024-12-13 22:59:28 -0500

    Custom Gameplay Effect Context

---
+++ Source/AstralSpirit/Private/ASAbilityTypes.cpp
@@ -0,0 +1,6 @@
+﻿#include "ASAbilityTypes.h"
+
+bool FASGameplayEffectContext::NetSerialize(FArchive& Ar, UPackageMap* Map, bool& bOutSuccess)
+{
+	return FGameplayEffectContext::NetSerialize(Ar, Map, bOutSuccess);
+}
---
+++ Source/AstralSpirit/Public/ASAbilityTypes.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+
+#include "GameplayEffectTypes.h"
+#include "ASAbilityTypes.generated.h"
+
+USTRUCT(BlueprintType)
+struct FASGameplayEffectContext : public FGameplayEffectContext
+{
+	GENERATED_BODY()
+
+public:
+
+	bool IsBlockedHit() { return bIsBlockedHit; }
+	bool IsCriticalHit() { return bIsCriticalHit; }
+
+	void SetIsBlockedHit(bool bInIsBlockedHit) { bIsBlockedHit = bInIsBlockedHit; }
+	void SetIsCriticalHit(bool bInIsCriticalHit) { bIsCriticalHit = bInIsCriticalHit; }
+	
+	/** Returns the actual struct used for serialization, subclasses must override this! */
+	virtual UScriptStruct* GetScriptStruct() const
+	{
+		return FGameplayEffectContext::StaticStruct();
+	}
+
+	/** Custom serialization, subclasses must override this */
+	virtual bool NetSerialize(FArchive& Ar, class UPackageMap* Map, bool& bOutSuccess);
+
+protected:
+
+	UPROPERTY()
+	bool bIsBlockedHit = false;
+
+	UPROPERTY()
+	bool bIsCriticalHit = false;
+	
+};
