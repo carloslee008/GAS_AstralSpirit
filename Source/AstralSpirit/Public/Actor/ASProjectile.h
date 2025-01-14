3a2304a46d7038b17bb5e0bb8dcff46430038054
carloslee008 <59149514+carloslee008@users.noreply.github.com>
2025-01-14 14:19:19 -0500

    Summoner Behavior Tree

--- Source/AstralSpirit/Public/Actor/ASProjectile.h
+++ Source/AstralSpirit/Public/Actor/ASProjectile.h
@@ -32,6 +32,9 @@ protected:
 
 	UFUNCTION()
 	void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USphereComponent> Sphere;
 	
 private:
 
@@ -39,9 +42,6 @@ private:
 	float LifeSpan = 10.f;
 	
 	bool bHit = false;
-	
-	UPROPERTY(VisibleAnywhere)
-	TObjectPtr<USphereComponent> Sphere;
 
 	UPROPERTY(EditAnywhere)
 	TObjectPtr<UNiagaraSystem> ImpactEffect;
