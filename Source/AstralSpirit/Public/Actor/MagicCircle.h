64b45885887819701681a345132c3de4760579ff
carloslee008 <59149514+carloslee008@users.noreply.github.com>
2025-03-26 00:00:04 -0400

    Magic Circle

---
+++ Source/AstralSpirit/Public/Actor/MagicCircle.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MagicCircle.generated.h"
+
+UCLASS()
+class ASTRALSPIRIT_API AMagicCircle : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	AMagicCircle();
+	virtual void Tick(float DeltaTime) override;
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UDecalComponent> MagicCircleDecal;
+
+
+
+};
