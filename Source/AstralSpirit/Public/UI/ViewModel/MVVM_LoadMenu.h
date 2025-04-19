939b913d44e8c961d0798afb46c3f831ccf83d41
carloslee008 <59149514+carloslee008@users.noreply.github.com>
2025-04-19 17:10:24 -0400

    Create View Model Class for Load Menu

---
+++ Source/AstralSpirit/Public/UI/ViewModel/MVVM_LoadMenu.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MVVMViewModelBase.h"
+#include "MVVM_LoadMenu.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASTRALSPIRIT_API UMVVM_LoadMenu : public UMVVMViewModelBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/AstralSpirit/Public/UI/Widget/LoadMenuWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "LoadMenuWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASTRALSPIRIT_API ULoadMenuWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
