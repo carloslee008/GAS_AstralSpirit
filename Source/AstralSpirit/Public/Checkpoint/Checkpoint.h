96c8a52a2c4342569d06ea2d5f5f0112df40c449
carloslee008 <59149514+carloslee008@users.noreply.github.com>
2025-04-24 19:47:00 -0400

    Activate Checkpoint

---
+++ Source/AstralSpirit/Public/Checkpoint/Checkpoint.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerStart.h"
+#include "Checkpoint.generated.h"
+
+class USphereComponent;
+/**
+ * 
+ */
+UCLASS()
+class ASTRALSPIRIT_API ACheckpoint : public APlayerStart
+{
+	GENERATED_BODY()
+public:
+	ACheckpoint(const FObjectInitializer& ObjectInitializer);
+
+protected:
+
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void StartGlowTimeline(UMaterialInstanceDynamic* DynamicMaterialInstance);
+
+	void Glow();
+
+private:
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UStaticMeshComponent> CheckpointMesh;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USphereComponent> Sphere;
+};
